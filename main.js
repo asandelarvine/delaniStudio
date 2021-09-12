4bf5bfed9444c7f7306d0636936ec56ffb36c0f5
asandelarvine <larvine.asande@student.moringaschool.com>
2021-09-12 04:21:37 +0300

    changes made

---
+++ main.js
@@ -0,0 +1,38 @@
+$(document).ready(function (){
+    $("text1").hide();
+    $("text2").hide();
+    $("text3").hide();
+    $(".toggle1,.toggle1a").click(function(){
+        $("#text1").toggle();
+        $(".toggle1").toggle();
+
+    });
+    $(".toggle2,.toggle2a").click(function(){
+        $("#text2").toggle();
+        $(".toggle2").toggle();
+
+    });
+    $(".toggle3,.toggle3c").click(function(){
+        $("#text3").toggle();
+        $(".toggle3").toggle();
+
+    });
+
+});
+//portfolio
+/*$(document).ready(function (){
+    $("myportfolio").hover(
+        function (){
+            $(this)
+            .find("project")
+            .show();
+        },
+        function (){
+            $(this)
+            .find("project")
+            .show();
+        }
+
+    );
+});*/
+    
